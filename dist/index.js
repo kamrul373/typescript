fba8505b96929a7a9a9de4b7e5d4cd0f6a743a14
kamrul373 <kamrul@lemonhive.com>
2023-05-06 16:34:32 +0600

    initialized

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var x = "Hello World";
+console.log(x, "typescript");
+const user = [1, "abul_1", 880171222];
+console.log(user);
+const array = ["ami", "tumi", "amara"];
+const userInfo = {
+    name: "kamrul hasan",
+    role: "Frontend developer",
+    company: "Lemon hive",
+};
+console.log(userInfo);
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+var x = "Hello World";
+console.log(x, "typescript" );
+
+const user : [number,string,number] = [1,"abul_1", 880171222];
+console.log(user);
+
+const array : string[] = ["ami", "tumi", "amara"];
+
+const userInfo : {
+	name : string,
+	role : string,
+	readonly company ? : "Lemon hive",
+} = {
+	name : "kamrul hasan",
+	role : "Frontend developer",
+	company : "Lemon hive",
+}
+
+console.log(userInfo);
