47348db9baf6e1b7a1a9a2e61fbee1210af7a2f3
textile.zero@gmail.com <kamrul@lemonhive.com>
2023-05-07 11:02:45 +0600

    typescript configured

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,26 @@
 "use strict";
 var x = "Hello World";
 console.log(x, "typescript");
+// string //array
 const user = [1, "abul_1", 880171222];
 console.log(user);
 const array = ["ami", "tumi", "amara"];
+// object
 const userInfo = {
     name: "kamrul hasan",
     role: "Frontend developer",
     company: "Lemon hive",
 };
 console.log(userInfo);
+// function 
+const add = (a, b) => a + b;
+const account = {
+    name: "kamrul",
+    id: 2,
+    balance: 120,
+    deposit(money) {
+        return `Your new balance is ${this.balance + money} taka`;
+    }
+};
+const newbalance = account.deposit(30);
+console.log(newbalance);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 var x = "Hello World";
 console.log(x, "typescript" );
-
+// string //array
 const user : [number,string,number] = [1,"abul_1", 880171222];
 console.log(user);
-
 const array : string[] = ["ami", "tumi", "amara"];
-
+// object
 const userInfo : {
 	name : string,
 	role : string,
@@ -15,5 +14,25 @@ const userInfo : {
 	role : "Frontend developer",
 	company : "Lemon hive",
 }
-
 console.log(userInfo);
+// function 
+
+const add = (a:number,b:number) :number  => a+b ;
+
+const account : {
+	name : string,
+	id : number,
+	balance : number,
+	deposit(money:number):string
+} = {
+	name : "kamrul",
+	id : 2,
+	balance : 120,
+	deposit(money:number) {
+		return `Your new balance is ${this.balance + money } taka`
+	}
+}
+
+const newbalance = account.deposit(30);
+
+console.log(newbalance);
